2bed2604f34b06b45331fddc6bb1e1301410ca2f
oscarjm97 <terceroonceoscar@gmail.com>
2020-03-07 16:15:01 +0100

    Creación de Componentes Registro e IndexTurista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { IndexturistaComponent } from './indexturista/indexturista.component';
 
 const routes: Routes = [
-  {path:'login',component: LoginComponent}
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'indexturista', component: IndexturistaComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { IndexturistaComponent } from './indexturista/indexturista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistroComponent,
+    IndexturistaComponent
   ],
   imports: [
     BrowserModule,
