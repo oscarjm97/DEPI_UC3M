a6e6e122efc383f2a7303a0a44b1308b2df0e070
ccdiegob95 <ccdiegob@gmail.com>
2020-04-05 18:56:38 +0200

    Registro con AngularMaterial

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.scss']
+  selector: "app-registro",
+  templateUrl: "./registro.component.html",
+  styleUrls: ["./registro.component.scss"],
 })
 export class RegistroComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
