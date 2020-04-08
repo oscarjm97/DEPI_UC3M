ed970ec394a4db386f5b685802d9e8f95f97acbc
oscarjm97 <terceroonceoscar@gmail.com>
2020-04-08 22:42:27 +0200

    Resolución de conflictos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,27 +3,18 @@ import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { RegistroComponent } from "./registro/registro.component";
 import { IndexturistaComponent } from "./indexturista/indexturista.component";
-<<<<<<< HEAD
 import { MainNavComponent } from "./main-nav/main-nav.component";
-=======
 import { ExperienceComponent } from "./experience/experience.component";
 import { ProfileComponent } from "./profile/profile.component";
->>>>>>> a4eb7acbf0d82f86247d2bc8cc74617f9bf49887
 
 const routes: Routes = [
   { path: "index", component: IndexturistaComponent },
   { path: "login", component: LoginComponent },
-<<<<<<< HEAD
+  { path: "signup", component: RegistroComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: "registro", component: RegistroComponent },
-  { path: "indexturista", component: IndexturistaComponent },
-  { path: "main-nav", component: MainNavComponent }
-=======
-  { path: "", redirectTo: "/index", pathMatch: "full" },
-  { path: "sigup", component: RegistroComponent },
+  { path: "main-nav", component: MainNavComponent },
   { path: "experience", component: ExperienceComponent },
   { path: "profile", component: ProfileComponent },
->>>>>>> a4eb7acbf0d82f86247d2bc8cc74617f9bf49887
 ];
 
 @NgModule({
