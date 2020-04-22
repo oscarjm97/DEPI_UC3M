346b777161bdeece5670ca8da8c0eee7b385c632
oscarjm97 <terceroonceoscar@gmail.com>
2020-04-22 21:28:31 +0200

    Cambios en el formulario de Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +11,15 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   hide: boolean;
-  
-  constructor(private router: Router) {}
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.hide = true;
   }
 
-  onSubmit() {
-
+  onSubmit(value) {
+    console.log("USER INFORMATION: " + value);
+    this.router.navigate(['index']);
   }
 }
