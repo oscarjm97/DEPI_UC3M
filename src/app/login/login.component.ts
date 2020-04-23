2b6c31b5803554870720e3ec3538b5c1f79e6c9a
oscarjm97 <terceroonceoscar@gmail.com>
2020-04-23 16:37:39 +0200

    Completado formulario de Login y Registro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: "app-login",
@@ -11,9 +10,22 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   hide: boolean;
+  loginForm: FormGroup;
+  validation_messages = {
+    'user': [
+      { type: 'required', message: 'User is required.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.' }
+    ]
+  };
 
-  constructor(private router: Router, private renderer: Renderer2) {
+  constructor(private router: Router, private renderer: Renderer2, fb: FormBuilder) {
     //this.renderer.setStyle(document.body, "background", "#FFF");
+    this.loginForm = fb.group({
+      user: ["", Validators.required],
+      password: ["", Validators.required]
+    });
   }
 
   ngOnInit() {
@@ -21,12 +33,12 @@ export class LoginComponent implements OnInit {
     document.body.className = "bg-body";
   }
 
-  onSubmit(value) {
-    console.log("USER INFORMATION: " + value);
-    this.router.navigate(['index']);
-  }
-
   ngOnDestroy() {
     document.body.className = "";
+  }
+
+  onSubmit(value) {
+    console.log(value);
+    this.router.navigate(['index']);
   }
 }
--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Renderer2 } from "@angular/core";
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: "app-registro",
@@ -6,15 +8,52 @@ import { Component, OnInit, Renderer2 } from "@angular/core";
   styleUrls: ["./registro.component.scss"],
 })
 export class RegistroComponent implements OnInit {
+  
   hide: boolean;
+  signupForm: FormGroup;
+  validation_messages = {
+    'name': [
+      { type: 'required', message: 'Name is required.' }
+    ],
+    'surname': [
+      { type: 'required', message: 'Surname is required.' }
+    ],
+    'email': [
+      { type: 'required', message: 'Email is required.' }
+    ],
+    'user': [
+      { type: 'required', message: 'User is required.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.' }
+    ],
+    'usertype': [
+      { type: 'required', message: 'Choose a type of user' }
+    ]
+  };
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private router: Router, private renderer: Renderer2, fb: FormBuilder) {
+    this.signupForm = fb.group({
+      name: ["", Validators.required],
+      surname: ["", Validators.required],
+      email: ["", Validators.required],
+      user: ["", Validators.required],
+      password: ["", Validators.required],
+      usertype: ["", Validators.required]
+    });
+  }
 
   ngOnInit() {
     this.hide = true;
     document.body.className = "bg-body";
   }
+
   ngOnDestroy() {
     document.body.className = "";
+  }
+
+  onSubmit(value) {
+    console.log(value);
+    this.router.navigate(['index']);
   }
 }
