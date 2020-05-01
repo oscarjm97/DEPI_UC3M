852e05c74d4898b0f531e0efd7ce2a62a1d8921e
ccdiegob95 <ccdiegob@gmail.com>
2020-05-01 11:40:28 +0200

    url photo in index

--- src/app/model/Experience.ts
+++ src/app/model/Experience.ts
@@ -1,23 +1,20 @@
-export interface IExperience{
-    name:string;
-    description:string;
-    price:number;
-    type:number;
-    rate:number;
-    
+export interface IExperience {
+  name: string;
+  description: string;
+  price: number;
+  type: number;
+  rate: number;
+  photo: string;
 }
-export class Experience implements IExperience
-{
-    name: string;
-    description: string;
-    price: number;
-    type: number;
-    rate: number;
+export class Experience implements IExperience {
+  name: string;
+  description: string;
+  price: number;
+  type: number;
+  rate: number;
+  photo: string;
 
-
-    constructor(o?:IExperience)
-    {
-        if(o !== undefined) Object.assign(this,o);
-    }   
-
+  constructor(o?: IExperience) {
+    if (o !== undefined) Object.assign(this, o);
+  }
 }
