38fdfe9828cc670c24d8146931f86002d54b8232
ccdiegob95 <ccdiegob@gmail.com>
2020-05-01 15:20:17 +0200

    enum in filter and href in navbar

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,12 +1,34 @@
 import { Component, OnInit } from "@angular/core";
 
+export enum TypesExperience {
+  naturaleza,
+  bar,
+  gastronomica,
+  piscina,
+  deportes,
+  cultura,
+  musica,
+}
+
 @Component({
   selector: "app-filter",
   templateUrl: "./filter.component.html",
   styleUrls: ["./filter.component.scss"],
 })
 export class FilterComponent implements OnInit {
+  public selectedExperience: string = "";
+  typesExperiences = TypesExperience;
+
   constructor() {}
 
   ngOnInit() {}
+
+  public onSelectExperience(typeExp: TypesExperience) {
+    this.selectedExperience = TypesExperience[typeExp];
+    console.log(this.selectedExperience);
+  }
+
+  public onClear() {
+    this.selectedExperience = "";
+  }
 }
--- src/app/model/Experience.ts
+++ src/app/model/Experience.ts
@@ -2,7 +2,7 @@ export interface IExperience {
   name: string;
   description: string;
   price: number;
-  type: number;
+  type: string;
   rate: number;
   photo: string;
 }
@@ -10,7 +10,7 @@ export class Experience implements IExperience {
   name: string;
   description: string;
   price: number;
-  type: number;
+  type: string;
   rate: number;
   photo: string;
 
