66114f7211e19b6c8fe62e7d794295bb7b4ade7e
ccdiegob95 <ccdiegob@gmail.com>
2020-05-05 19:36:51 +0200

    milestones

--- src/app/services/experience/experience.service.ts
+++ src/app/services/experience/experience.service.ts
@@ -3,6 +3,9 @@ import { User } from "src/app/model/User";
 import { Experience } from "src/app/model/Experience";
 import { map, filter } from "rxjs/operators";
 import { Observable } from "rxjs";
+import { AchievementService } from "./../achievement/achievement.service";
+import { Achievement } from "./../../model/Achievement";
+import { AuthService } from "./../../shared/auth.service";
 import {
   AngularFirestoreCollection,
   AngularFirestore,
@@ -15,8 +18,13 @@ export class ExperienceService {
   defaultPhoto: string =
     "https://pngimage.net/wp-content/uploads/2018/06/paisaje-png-2.png";
   public afs: AngularFirestoreCollection<Experience>;
+  achievement: Achievement;
 
-  constructor(private firestore: AngularFirestore) {
+  constructor(
+    private firestore: AngularFirestore,
+    private achievementService: AchievementService,
+    private authService: AuthService
+  ) {
     this.afs = this.firestore.collection("experiences");
   }
 
@@ -51,11 +59,23 @@ export class ExperienceService {
     if (exp.photo == null || exp.photo == "") {
       exp.photo = this.defaultPhoto;
     }
+
+    if (this.authService.checkMilestone(user, "1") == false) {
+      await this.assignMilestone(user);
+    }
+
     return this.afs
       .doc(exp.id)
       .set({ ...exp })
       .then((r) => {
         return exp.id;
       });
+  }
+
+  public async assignMilestone(user: User) {
+    this.achievement = await this.achievementService.getById("1");
+    this.achievement.id = "1";
+    user.milestones.push(this.achievement);
+    await this.authService.updateUserById(user);
   }
 }
